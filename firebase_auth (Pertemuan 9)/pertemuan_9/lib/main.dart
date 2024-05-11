0d64bc8ac9bbb110c6865507de21d87c4eb9a6b2
Fakhir Mustafa Afdal <138569742+FakhirMustafa@users.noreply.github.com>
2024-05-11 19:53:33 +0700

    Pertemuan 9

---
+++ firebase_auth (Pertemuan 9)/pertemuan_9/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'package:pertemuan_9/bloc/login/login_cubit.dart';
+import 'package:pertemuan_9/ui/splash.dart';
+import 'package:pertemuan_9/utils/routes.dart';
+import 'bloc/register/register_cubit.dart';
+import 'firebase_options.dart'; // Assuming this contains DefaultFirebaseOptions.currentPlatform
+import 'utils/routes.dart'; // Assuming this contains generateRoute and NAV_KEY
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    print('Failed to initialize Firebase: $e');
+    // Handle Firebase initialization error here
+  }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => RegisterCubit()),
+      ],
+      child: MaterialApp(
+        title: "Praktikum 9",
+        debugShowCheckedModeBanner: false,
+        navigatorKey: NAV_KEY,
+        onGenerateRoute: generateRoute,
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
